d6faf8856355e0222b32f552d374b62ce9282a35
NickNinjaGit <balguing@gmail.com>
2024-11-27 12:14:43 -0300

    first commit

---
+++ backend/helpers/create-user-token.js
@@ -0,0 +1,18 @@
+const jwt = require('jsonwebtoken')
+
+const createUserToken = async(user, req, res) => {
+    // create a token
+    const token = jwt.sign({
+        name: user.name,
+        id: user._id
+    }, "secret")
+
+    // return token
+    res.status(200).json({
+        message: "Usuário autenticado",
+        token: token,
+        userId: user._id,
+    })
+}
+
+module.exports = createUserToken
